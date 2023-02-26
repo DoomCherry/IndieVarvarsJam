a2f948d3cd5285c5333ad806135a5e89799d0e55
Anatoliy Varavko <47712375+DoomCherry@users.noreply.github.com>
2023-02-26 21:46:43 +0500

    Fix

--- Assets/ChildController.cs
+++ Assets/ChildController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class ChildController : MonoBehaviour
 {
-    public int _activateCountOnAwake = 3;
+    public int _activateCountMaxOnAwake = 3;
+    public int _activateCountMinOnAwake = 0;
 
     private void Awake()
     {
+        int activateCount = Random.Range(_activateCountMinOnAwake, _activateCountMaxOnAwake + 1);
+
         GameObject[] children = new GameObject[transform.childCount];
 
         for (int i = 0; i < transform.childCount; i++)
@@ -16,7 +19,7 @@ public class ChildController : MonoBehaviour
             children[i] = transform.GetChild(i).gameObject;
         }
 
-        GameObject[] activateChildren = children.OrderBy(x => Random.Range(0, transform.childCount)).Take(3).ToArray();
+        GameObject[] activateChildren = children.OrderBy(x => Random.Range(0, transform.childCount)).Take(activateCount).ToArray();
 
         for (int i = 0; i < activateChildren.Length; i++)
         {
